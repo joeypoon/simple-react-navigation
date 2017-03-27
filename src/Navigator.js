1f0cf7552666a57e1a419db11786757b8e1693c3
Joey F. Poon <joey@kaijudev.com>
2017-03-26 20:07:40 -0500

    Add reducer creator.

--- src/Navigator.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Navigator(props) {
-	function currentRoute() {
-		return props.routeStack.slice(-1)[0];
-	}
-
-	return props.routes[currentRoute()].screen(props);
-}
