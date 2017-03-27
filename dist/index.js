a2f9e208e75080c931217e4e52b635f7641e0967
Joey F. Poon <joey@kaijudev.com>
2017-03-26 20:10:55 -0500

    Unignore dist. Add dist.

---
+++ dist/index.js
@@ -0,0 +1,23 @@
+export default function Navigator(props) {
+	function currentRoute() {
+		return props.routeStack.slice(-1)[0];
+	}
+
+	return props.routes[currentRoute()].screen(props);
+}
+
+export function createReducer(options) {
+	const { defaultRoute } = options;
+	const NAVIGATE = 'NAVIGATE';
+
+	function reducer(state = [defaultRoute], action) {
+		switch (action.type) {
+			case NAVIGATE:
+				return state.concat(action.route);
+			default:
+				return state;
+		}
+	}
+
+	return reducer;
+}
