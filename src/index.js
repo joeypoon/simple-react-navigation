c79141616e0ece06cd221a4f6719ea31fd0880b9
Joey F. Poon <joey@kaijudev.com>
2017-04-01 16:42:57 -0500

    Extend React.Component.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
-import { createElement } from 'react';
+import React, { createElement } from 'react';
 
-export default function Navigator(props) {
-	function currentRoute() {
+export default class Navigator extends React.Component {
+	currentRoute() {
 		return props.routeStack.slice(-1)[0];
 	}
 
-	return createElement(props.routes[currentRoute()].screen, props);
+	render() {
+		const { routes } = this.props;
+		return createElement(routes[this.currentRoute()].screen, props);
+	}
 }
 
 export function createReducer(options) {
 	const { defaultRoute } = options;
 	const NAVIGATE = 'NAVIGATE';
+	const NAVIGATE_BACK = 'NAVIGATE_BACK';
 
 	function reducer(state = [ defaultRoute ], action) {
 		switch (action.type) {
 			case NAVIGATE: return state.concat(action.route);
+			case NAVIGATE_BACK: return state.slice(0, -1);
 			default: return state;
 		}
 	}
